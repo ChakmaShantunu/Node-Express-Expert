7740e92a0f3db7d9a0ababe5f12aec815360cd89
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-22 22:24:51 +0600

    create and delete files using fs (sync and async) with error handling

--- delete-file.js
+++ delete-file.js
@@ -3,8 +3,28 @@ const fs = require("fs");
 fs.writeFileSync("./output/temporary.txt", "this is a temp file");
 console.log("temp created");
 
-if(fs.existsSync("./output/temporary.txt")) {
+if (fs.existsSync("./output/temporary.txt")) {
     console.log("file exist");
     fs.unlinkSync("./output/temporary.txt");
     console.log("file deleted");
 }
+
+try {
+    fs.unlinkSync("./output/temporary.txt")
+} catch (error) {
+    console.log(error.message, "Error");
+}
+
+fs.writeFile("./output/temp2.txt", "another temp file", (err) => {
+    if (err) return console.error(err.message);
+
+    console.log("another temp file created");
+
+    fs.unlink("./output/temp2.txt", (err) => {
+        if (err) {
+            console.error("Error:", err.message);
+        } else {
+            console.log("temp2 deleted")
+        }
+    })
+})
