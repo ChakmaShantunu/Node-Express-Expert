016c7ea6a4454362f6b08df6d09c608f950f4d82
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-24 22:24:28 +0600

    implement full CLI interface with init, organize commands and category-based file organizer

--- file-organizer.js
+++ file-organizer.js
@@ -70,15 +70,84 @@ function initializeDirectories() {
 
 function getCategory(fileName) {
     const ext = path.extname(fileName).toLowerCase();
-    for(const [category, extensions] of Object.entries(categories))
-    {
-        if(extensions.includes(ext)) {
+    for (const [category, extensions] of Object.entries(categories)) {
+        if (extensions.includes(ext)) {
             return category;
         }
     }
 
     return "others";
 }
+
+function organizeFiles() {
+    console.log("file organizer \n");
+    console.log("source: ", sourdeDir);
+    console.log("Destination: ", organizedDir);
+    console.log("\n" + "-".repeat(50) + "\n");
+
+    const files = fs.readdirSync(sourdeDir);
+
+    if (files.length === 0) {
+        console.log("No files to work on!!!");
+
+        return
+    }
+    console.log(`Found ${files.length} files to organize`);
+
+    const stats = {
+        total: 0,
+        byCategory: {},
+    }
+
+    files.forEach(file => {
+        const sourcePath = path.join(sourdeDir, file)
+        const stat = fs.statSync(sourcePath);
+
+        if (stat.isDirectory()) {
+            return;
+        }
+        const category = getCategory(file);
+        const destDir = path.join(organizedDir, category);
+        const destPath = path.join(destDir, file);
+
+        fs.copyFileSync(sourcePath, destPath);
+
+        stats.total++;
+        stats.byCategory[category] = (stats.byCategory[category] || 0) + 1
+
+        console.log(`${file}`);
+        console.log(`${category}`);
+        console.log(`${stat.size}`);
+    });
+
+}
+
+function showHelp() {
+    console.log(`file organizer - usage: 
+        commands: 
+        init -create files
+        organize - organize files into categories
+        example:
+        node file-organizer init
+        node file-organizer organize
+        `)
+}
+
+const command = process.argv[2];
+
+switch (command) {
+    case "init":
+        initializeDirectories();
+        break;
+    case "organize":
+        organizeFiles()
+        break
+    default:
+        showHelp()
+        break;
+}
+
+
 
 // initializeDirectories();
 
