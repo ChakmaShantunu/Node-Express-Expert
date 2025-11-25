1657c254179afb67dca2a4b3d5e93ad0ad5ade28
shantunu007 <chakmashantunu.web@gmail.com>
2025-11-25 09:39:21 +0600

    implement SHA-512 hashing example with Node.js crypto modul

--- hashing.js
+++ hashing.js
@@ -4,3 +4,9 @@ const crypto = require("crypto");
 const sha256Hash = crypto.createHash("sha256").update("password123").digest("hex");
 console.log("input: password123");
 console.log("sha256 password: ", sha256Hash);
+
+const sha512Hash = crypto.createHash("sha512").update("password123").digest("hex");
+console.log("input: password123");
+console.log("sha512 password: ", sha512Hash);
+
+console.log((2 ** 32 / 10 ** 9));
